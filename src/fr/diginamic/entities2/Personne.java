e93fa72d8407dd025babac74ec8b2b6f296b5907
Theo Le Moal <contact@theo-le-moal.fr>
2023-05-17 15:20:05 +0200

    TP 2 fini

--- src/fr/diginamic/entities2/Personne.java
+++ src/fr/diginamic/entities2/Personne.java
@@ -7,6 +7,19 @@ public class Personne {
     public String prenom;
     public AdressePostale adr;
 
+    public Personne() {
+        this.nom = "Doe";
+        this.prenom = "Jhon";
+    }
+
+    public Personne(String nom, String prenom, AdressePostale adr) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adr = adr;
+    }
+
+
+
     @Override
     public String toString() {
         return "Personne{" +
